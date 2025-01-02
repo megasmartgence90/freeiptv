14a9b86979d6768a7c061d74efcc9c6157b204d3
MEGA-GROUP <uzagdasan@mail.ru>
2025-01-03 02:39:30 +0400

    Delete ressources/kuw directory

--- ressources/kuw/kuwspo.py
+++
@@ -1,8 +0,0 @@
-import streamlink
-import requests
-streams = streamlink.streams('https://www.media.gov.kw/LiveTV.aspx?PanChannel=KTVSports')
-master = streams["best"].multivariant.uri
-s = requests.Session()
-response = s.get(master)
-content = response.text
-print(content)
