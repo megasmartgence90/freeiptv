abff46a28d7037a620ee8f68554c92d3f5233001
MEGA-GROUP <uzagdasan@mail.ru>
2025-01-03 02:38:58 +0400

    Delete ressources/ftv directory

--- ressources/ftv/py/fr2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/ftv/py/fr.py > ressources/ftv/py/fr2.m3u8
-
-exit 0
--- ressources/ftv/py/frsr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/ftv/py/frsr.py > ressources/ftv/py/frsr.m3u8
-
-exit 0
